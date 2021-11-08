4478a24ef33ce213aa7001bbe38c9989e7ab65f0
Moshe Yaniv <moshe.yaniv@gmail.com>
2021-11-08 23:50:12 +0200

    first test

--- docker/gdal/scripts/app.py
+++ docker/gdal/scripts/app.py
@@ -7,9 +7,11 @@ import os
 # check if we have args in the python call eg : app.py path_to_shp_folder path_to_json_folder
 if len(sys.argv)==2 : 
     # no paths found so we are going to set the default path of the docker \tmp
-    pass
+    print ('arguments where not found: (path to shape file or path to json file)')
+    #sys.exit()
     
 else:
+    # for debug
     path_to_shp_folder = r"\tmp\Shape"
     path_to_json_folder = r"\tmp\GeoJSON"
         
@@ -18,6 +20,7 @@ else:
 for f in os.listdir(path_to_shp_folder) :
     if f.split('.')[-1] = 'shp'
     path_to_shp_file = path_to_shp_folder + "//" + f
+    # the name of the json file is the same name as the shp file
     path_to_json_file = path_to_json_folder + "//" + f[0] + '.json' 
     cmd = 'ogr2ogr -f GeoJSON"
     subprocess.run([cmd, path_to_json_file, path_to_shp_file], capture_output=True)
